4afb3b6e8c8041e7988d9a012d1093ff4413dc3d
Teddy10000 <tedakrong12@gmail.com>
2023-11-25 15:24:35 +0000

    added the dat.gui to change colors

--- EXERCISE2/script.js
+++ EXERCISE2/script.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui'
 
 const renderer = new THREE.WebGL1Renderer();
 
@@ -15,11 +17,26 @@ const camera = new THREE.PerspectiveCamera(
     1000
 
 ); 
+
+//adding mobility to the camera
+const orbit = new OrbitControls(camera,renderer.domElement) 
+
 // setting position now we can do all together with the
 //camera.position.z = 5
 camera.position.set(0,2,5)
+//TO UPDATE THE ORBIT EVERY MOVEMENT
+orbit.update()
 //AXES HELPER TO HEP WITH THE AXES
 const axesHelper = new THREE.AxesHelper(5)
+
+//FOR CHANGING THE COLOR OF THE GUI
+const gui = new dat.GUI()
+const options = {
+  boxColor : '#ffea00'
+};
+gui.addColor(options,'boxColor').onChange(function(e){
+    box.material.color.set(e)
+})
 
 //addding a geometry mesh element that is a box so the geometry that is the shape and the material
 
@@ -29,10 +46,22 @@ const box =  new THREE.Mesh(boxGeometry,boxMaterial)
 scene.add(box)
 scene.add(axesHelper)
 
-//writing the animation function
-function animations(){
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
+//adding a GRID HELPER TO GRID AND VALUES ADDED TO INCREASE THE SURFACE OF THE GRID AND SECND PARAMETER TO DIVIDE THE GRID
+const gridHelper = new THREE.GridHelper(30)
+scene.add(gridHelper)
+
+//CREATING ANOTHER GEOMETRY 
+const planeGeometry = new THREE.PlaneGeometry(30,30);
+const planeMaterial = new THREE.MeshBasicMaterial({color:0xFFFFFF})
+const plane = new THREE.Mesh(planeGeometry,planeMaterial) 
+plane.rotation.x = -0.5 * Math.PI
+scene.add(plane)
+
+
+//writing the animation function to rotate the element
+function animations(time){
+    box.rotation.x += 0.01  ;
+    box.rotation.y += 0.01  ;
     renderer.render(scene,camera)
 }
 renderer.setAnimationLoop(animations);
